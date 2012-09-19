673330d5cfa08d8c0ff8ba61456b95d22195e63d
Gustavo <knx@natur.lacosox.org>
2012-09-19 00:01:36 -0300

    agregando analitycs y eliminado archivos innecesarios

--- template.php
+++ template.php
@@ -77,7 +77,15 @@
                 </div>
               </div>
 	    </form>
-
+<!-- AddThis Button BEGIN -->
+<div class="addthis_toolbox addthis_default_style ">
+<a class="addthis_button_facebook_like" fb:like:layout="button_count"></a>
+<a class="addthis_button_tweet"></a>
+<a class="addthis_counter addthis_pill_style"></a>
+</div>
+<script type="text/javascript">var addthis_config = {"data_track_addressbar":true};</script>
+<script type="text/javascript" src="http://s7.addthis.com/js/300/addthis_widget.js#pubid=ra-4e4eb2e17c08f43b"></script>
+<!-- AddThis Button END -->
             </div>
 
             <hr>
@@ -104,7 +112,7 @@
         <script src="js/main.js"></script>
 
         <script>
-            var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+            var _gaq=[['_setAccount','UA-34911399-1'],['_trackPageview']];
             (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
             g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
             s.parentNode.insertBefore(g,s)}(document,'script'));
